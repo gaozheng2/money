a570c121901bc9f60af6cbd104b8ec9b6b478a0f
coolcard <gaoz@sina.com>
2021-02-18 21:24:59 +0800

    添加自动部署 sh 文件

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 const path = require('path')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/money/'
+    ? '/money-website/'
     : '/',
   lintOnSave: false,
   chainWebpack: (config) => { // 配置 webpack 参数
