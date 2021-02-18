2d47131dd39e79b88f4ec2eacde9106566c8bbda
coolcard <gaoz@sina.com>
2021-02-18 20:43:58 +0800

    tags 返回值重写

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout>
     <Tabs
-      :data-source="TYPE_LIST"
-      :value.sync="typeValue"
-      class-prefix="types"
+        :data-source="TYPE_LIST"
+        :value.sync="typeValue"
+        class-prefix="types"
     />
     <!-- <Tabs
       :data-source="DATE_LIST"
@@ -28,87 +28,80 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Types from '@/views/Money/Types.vue'
-import { Component } from 'vue-property-decorator'
-import Tabs from '@/components/Tabs.vue'
-import { TYPE_LIST, DATE_LIST } from '@/lib/constants'
-import dayjs from 'dayjs'
+import Vue from 'vue';
+import Types from '@/views/Money/Types.vue';
+import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
+import {TYPE_LIST, DATE_LIST} from '@/lib/constants';
+import dayjs from 'dayjs';
 
 @Component({
-  components: { Tabs, Types },
+  components: {Tabs, Types},
 })
 export default class Statistics extends Vue {
-  dateValue = 'day'
-  typeValue = '-'
-  TYPE_LIST = TYPE_LIST
-  DATE_LIST = DATE_LIST
+  dateValue = 'day';
+  typeValue = '-';
+  TYPE_LIST = TYPE_LIST;
+  DATE_LIST = DATE_LIST;
 
   tagString(tags: Tag[]) {
-    let result = ''
-    if (tags) {
-      tags.forEach((item) => {
-        if (result) {
-          result = result + ', ' + item.name
-        } else {
-          result = item.name
-        }
-      })
+    if (tags.length === 0) {
+      return '无';
+    } else {
+      return tags.map(t => t.name).join(', ');
     }
-    if (!result) result = '无'
-    return result
   }
 
   day(str: string) {
-    const d = dayjs(str)
-    const now = dayjs()
+    const d = dayjs(str);
+    const now = dayjs();
     if (d.isSame(now, 'day')) {
-      return '今天'
+      return '今天';
     } else if (d.isSame(now.subtract(1, 'day'), 'day')) {
-      return '昨天'
+      return '昨天';
     } else if (d.isSame(now.subtract(2, 'day'), 'day')) {
-      return '前天'
+      return '前天';
     } else if (d.isSame(now.subtract(3, 'day'), 'day')) {
-      return '大前天'
+      return '大前天';
     } else if (d.isSame(now, 'year')) {
-      return d.format('M月D日')
+      return d.format('M月D日');
     } else {
-      return d.format('YYYY年M月D日')
+      return d.format('YYYY年M月D日');
     }
   }
 
   get moneyList() {
     return this.$store.state.moneyDataList
-      .filter((item: MoneyData) => item.type === this.typeValue)
-      .sort((a, b) => dayjs(b.date) - dayjs(a.date))
+        .filter((item: MoneyData) => item.type === this.typeValue)
+        .sort((a, b) => dayjs(b.date) - dayjs(a.date));
   }
 
   get result() {
-    if (this.moneyList.length === 0) return []
+    if (this.moneyList.length === 0) return [];
     const hashTable: { title: string; items: MoneyData[]; total?: number }[] = [
       {
         title: dayjs(this.moneyList[0].date).format('YYYY-MM-DD'),
         items: [this.moneyList[0]],
       },
-    ]
+    ];
 
     for (let i = 1; i < this.moneyList.length; i++) {
-      const current = this.moneyList[i]
-      const last = hashTable[hashTable.length - 1]
+      const current = this.moneyList[i];
+      const last = hashTable[hashTable.length - 1];
       if (dayjs(last.title).isSame(dayjs(current.date), 'day')) {
-        last.items.push(current)
+        last.items.push(current);
       } else {
         hashTable.push({
           title: dayjs(current.date).format('YYYY-MM-DD'),
           items: [current],
-        })
+        });
       }
     }
 
     hashTable.forEach((group) => {
-      group.total = group.items.reduce((sum, item) => sum + item.num, 0)
-    })
-    return hashTable
+      group.total = group.items.reduce((sum, item) => sum + item.num, 0);
+    });
+    return hashTable;
   }
 }
 </script>
